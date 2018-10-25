6a984f9016c2666e9c963eb88d468d8c45b9ee40
Morawski <Pawel.Morawski@dnvgl.com>
2018-10-25 14:38:07 +0200

    Update TourneyKeeper/Startup.cs
    Test commit

--- TourneyKeeper/Startup.cs
+++ TourneyKeeper/Startup.cs
@@ -27,7 +27,7 @@ namespace TourneyKeeper
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("At some point of time there will be tournaments");
             });
         }
     }
