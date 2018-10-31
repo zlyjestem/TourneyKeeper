e51f7ac3708164b90e6e95e7cff8c7b84c284013
Morawski <Pawel.Morawski@dnvgl.com>
2018-10-31 14:38:22 +0100

    Projects set-up

---
+++ TK.Tournaments.WebAPI/Entities/Tournament.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourneyKeeper.Entities
+{
+    public class Tournament
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+        public string City { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public float Price { get; set; }
+        public int SwissRounds { get; set; }
+        public bool IfTopCut { get; set; }
+        public bool IfProgressiveCut { get; set; }
+        public string Venue { get; set; }
+        public string Adress { get; set; }
+    }
+}
