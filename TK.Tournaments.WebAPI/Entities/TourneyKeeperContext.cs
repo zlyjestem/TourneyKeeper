5c3e6dced482a9e374455efdf574028bb2ce6ace
Morawski <Pawel.Morawski@dnvgl.com>
2018-11-08 14:03:11 +0100

    DB context added

---
+++ TK.Tournaments.WebAPI/Entities/TourneyKeeperContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TourneyKeeper.Entities;
+
+namespace TK.Tournaments.WebAPI.Entities
+{
+    public class TourneyKeeperContext : DbContext
+    {
+        public TourneyKeeperContext(DbContextOptions<TourneyKeeperContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+        public DbSet<Tournament> Tournaments { get; set; }
+    }
+}
--- TK.Tournaments.WebAPI/Startup.cs
+++ TK.Tournaments.WebAPI/Startup.cs
@@ -6,7 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TK.Tournaments.WebAPI.Entities;
 
 namespace TourneyKeeper
 {
@@ -14,12 +17,21 @@ namespace TourneyKeeper
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public static IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
             services.AddMvc()
                 .AddMvcOptions(o => o.OutputFormatters.Add(
                     new XmlDataContractSerializerOutputFormatter()));
+
+            var connectionString = Startup.Configuration["connectionStrings:tourneyKeeperDBConnectionString"];
+            services.AddDbContext<TourneyKeeperContext>(o => o.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
